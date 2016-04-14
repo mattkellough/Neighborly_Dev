dfa1f81550f6ed50385d7ca6fb760388a24b5417
Matt Kellough <mrk10a@gmail.com>
2016-04-14 17:11:32 -0400

    add broken markers

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,9 +1,11 @@
 $(function() {
 
+  var center = new google.maps.LatLng(39.5, -98.35)
+
   var mapOptions = {
     zoom: 5,
-    center: new google.maps.LatLng(39.5, -98.35),
-    panControl: false,
+    center,
+    panControl: true,
     panControlOptions: {
       position: google.maps.ControlPosition.BOTTOM_LEFT
     },
@@ -15,6 +17,43 @@ $(function() {
     scaleControl: false
 
   };
+
+  var request = {
+    location: center,
+    radius: 8047,
+    types: ['cafe']
+  }
+  
+  function callback(results, status) {
+    if(status == google.maps.places/PlacesServiceStatus.OK) {
+      for (var i = 0; i < results.length; i++){
+        createMarker(results[i]);
+      }
+    }
+  }
+
+
+  var service = new google.maps.places.PlacesService(mapOptions);
+
+  service.nearbySearch(request, callback);
+
+  console.log('test');
+
+  // function callback(results, status) {
+  //   if(status == google.maps.places/PlacesServiceStatus.OK) {
+  //     for (var i = 0; i < results.length; i++){
+  //       createMarker(results[i]);
+  //     }
+  //   }
+  // }
+
+  // function createMarker(place) {
+  //   var placeLoc = place.geometry.location;
+  //   var marker = new google.maps.Marker({
+  //     map: map,
+  //     position: place.geometry.location
+  //   });
+  // }
 
   map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
