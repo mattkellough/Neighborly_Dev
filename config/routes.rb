26043b0d8c9af29a8aee98271f7a075f546649f0
Matt Kellough <mrk10a@gmail.com>
2016-04-15 01:30:36 -0400

    added logos to index

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'jobs#landing'
+  root 'jobs#index'
 
   # Example of regular route:
    get '/index' => 'jobs#index'
